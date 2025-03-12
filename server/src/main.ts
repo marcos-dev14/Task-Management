cbfc931c58bc20804282350013b70d7deaedc2b3
marcos-dev14 <mprogramador.10@gmail.com>
2025-03-11 21:18:37 -0300

    fix: ajuste na config do cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,11 +7,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-    exposedHeaders: ['Authorization'],
-  })
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  });
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
